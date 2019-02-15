4a3a2fc3eb7afca024d4d26ba6c7ae8d9f49dfad
Mads Okkels <mbokkels@gmail.com>
2019-02-15 22:39:25 +0900

    Added important link

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends Component {
                     </p>
                     <a
                         className="App-link"
-                        href="https://reactjs.org"
+                        href="https://www.facebook.com/messages/t/erik.poerksen/"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        Learn React
+                        Learn React by asking Erik
                     </a>
                 </header>
             </div>
