dc47e527b1bbc3bbbfdb0fbf8927000de0bf9281
Christian Dam <christian.dam@avidlyagency.com>
2019-02-16 11:10:37 +0900

    Secondstep initiiiial

--- src/steps/SecondStep.js
+++ src/steps/SecondStep.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Slider, Row, Col } from 'antd'; 
 
 export default () => {
     return (
-        <h1>Hi from second step</h1>
+        <div>
+            <Row type="flex" align="middle">
+                <Col span={8}>
+                    <Slider></Slider>
+                </Col>
+            </Row>
+        </div>
     );
 }
