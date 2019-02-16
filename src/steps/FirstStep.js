9a89f83321e9bf6c2b7d9993b430462f1701eded
Mads Okkels <mbokkels@gmail.com>
2019-02-16 12:10:30 +0900

    Body

--- src/steps/FirstStep.js
+++ src/steps/FirstStep.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Checkbox, Row, Col } from 'antd';
+import body from './body.svg';
 
 export default () => {
+
+    function onChange(c) {
+        console.log('checked = ', c);
+    }
+
     return (
+        <div>
         <h1>Hi from first step</h1>
+            <image>source={require(body)}</image>
+            <Checkbox.Group style={{ width: '100%' }} onChange={onChange}>
+                <Row>
+                    <Col span={8}><Checkbox value="A">A</Checkbox></Col><br></br>
+                    <Col span={8}><Checkbox value="B">B</Checkbox></Col>
+                    <Col span={8}><Checkbox value="C">C</Checkbox></Col>
+                    <Col span={8}><Checkbox value="D">D</Checkbox></Col>
+                    <Col span={8}><Checkbox value="E">E</Checkbox></Col>
+                </Row>
+            </Checkbox.Group>
+        </div>
     );
 }
