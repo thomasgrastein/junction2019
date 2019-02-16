d003423a5ee92854a95ce3babfc219038a2eaf1a
Christian Dam <christian.dam@avidlyagency.com>
2019-02-16 12:27:16 +0900

    second step

--- src/steps/SecondStep.js
+++ src/steps/SecondStep.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { Slider, Row, Col } from 'antd'; 
+import { Slider, Row, Col, Checkbox, Select } from 'antd'; 
 
 export default () => {
+
+    const CheckboxGroup = Checkbox.Group;
+    const plainOptions = ['Yes', 'No'];
+    const Option = Select.Option;
+
+    function onChange(checkedValues) {
+        console.log('checked = ', checkedValues);
+    }
+
+    const children = [];
+    for (let i = 10; i < 36; i++) {
+        children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+    }
+
+    function handleChange(value) {
+        console.log(`selected ${value}`);
+    }
+
     return (
         <div>
             <Row type="flex" align="middle">
                 <Col span={8}>
+                    <strong>How much pain to you experience (1 - 100)</strong>
                     <Slider></Slider>
+                    <strong>Have you experienced this before?</strong>
+                    <br></br>
+                    <CheckboxGroup options={plainOptions} onChange={onChange} />
+                    <br></br>
+                    <strong>When do you experince this problem?</strong>
+                    <Select
+                        mode="tags"
+                        style={{ width: '100%' }}
+                        placeholder="Tags Mode"
+                        onChange={handleChange}
+                    >
+                        {children}
+                    </Select>
                 </Col>
             </Row>
         </div>
